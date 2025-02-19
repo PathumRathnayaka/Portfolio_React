2de85547a780d35b7d2ebfcd8203060956e01823
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-02-19 18:14:45 +0530

    edit link

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -4,21 +4,24 @@ import { ArrowRight } from 'lucide-react';
 const articles = [
   {
     title: 'Microservice Architecture සිංහලෙන්',
-    excerpt: 'Learn best practices for building large-scale React applications with modern architecture.',
+    excerpt: 'සරලව පැහැදිලි කරොත් Microservices Architecture එකක් කියන්නෙ අපි ලග තියන මොකක් හෝ software application එකක් එක පාර සම්පුර්ණ ලොකු application එකක් විදිහට හදන් නැතුව කුඩා කුඩා කොටස් කිහිපයකට වෙන් කරලා',
     date: 'Mar 15, 2024',
     image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi5n9bpYud478tvbp2TxArV10ZPsDzXLmv0Q4TmReci54uNarwx0BRg_ljTIjLC5Yv6tRd-OwEkGiTqQMWwcf3o8q9fgSc76u3UGNVHesFBooBrvhy5aL9BUV8kfmMtmz-j36ncDcJ5T1iw3SwJpjEKXyAzj_Ljl42zddmd1yvtG3GmH5vZhMfMAjdwIRlF/w508-h305/643fda5329f14368ca72adaf_Example%20of%20a%20microservice.png',
+    link:'https://www.blogger.com/blog/post/edit/8514125923631382986/5390360106439228933'
   },
   {
     title: 'The Future of Web Development',
     excerpt: 'Exploring upcoming trends and technologies in web development for 2024 and beyond.',
     date: 'Mar 10, 2024',
     image: 'https://images.unsplash.com/photo-1504639725590-34d0984388bd?auto=format&fit=crop&w=800&q=80',
+    link:'https://www.blogger.com/blog/post/edit/8514125923631382986/5390360106439228933'
   },
   {
     title: 'මොනවද මෙ SOLID Principles කියන්නෙ?',
-    excerpt: 'A comprehensive guide to using TypeScript effectively in your projects.',
+    excerpt: 'SOLID Principle කියන්නෙ Object Oriented Programming System වල පාවිච්චි කරන guide lines. Developer කෙනෙක් විදිහට හෝ Software Engineer කෙනෙක් විදිහට මේවා පිලිපදින එක ඉතා වැදගත්.',
     date: 'Mar 5, 2024',
     image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiibbrQWTM58mZCta-ZFMhiRj0f_PPMtmQlQGpPYqfxBCbGQeLk-0WB3Jz-flWuEE6UT8go-a1AmTu7ZHBR3w-gYngMomWJab9cShgy3HYUNc3MB7Y8WADVeZLYuy28wVg-Aay3vBxd-7Fio0fUhJyMQs_R8cUjCQWV023wn1as1ukgm0JIjSVPAw7XeiBV/w610-h376/image%20(1).jpg',
+    link:'https://www.blogger.com/blog/post/edit/8514125923631382986/5390360106439228933'
   },
 ];
 
@@ -52,7 +55,7 @@ export default function Articles() {
                   {article.excerpt}
                 </p>
                 <a
-                  href="#"
+                  href={article.link}
                   className="inline-flex items-center text-teal-500 hover:text-teal-600 dark:text-teal-400 dark:hover:text-teal-300"
                 >
                   Read More
